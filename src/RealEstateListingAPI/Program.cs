e8137c212776186f0a5227ce9cd8ea9f7dd88a89
Thiago André de Almeida <thiagoalmeida@MacBook-Pro.local>
2026-01-14 22:28:22 -0300

    fix: enable swagger to run with docker - as a prod environment test

--- src/RealEstateListingAPI/Program.cs
+++ src/RealEstateListingAPI/Program.cs
@@ -44,12 +44,11 @@ builder.Services.AddSwaggerGen(c =>
 });
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Real Estate Listing API V1"));
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
